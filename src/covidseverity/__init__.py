52e257201d907fb9c7d9fa85634ea831618fc62c
Filipe Verri <filipeverri@gmail.com>
2021-06-15 11:03:21 -0300

    Configuring pages.

--- src/covidseverity/__init__.py
+++ src/covidseverity/__init__.py
@@ -1,6 +1,6 @@
 """
-Python package `covidseverity` to generate severity-prediction views of the
-datasets provided by FAPESP COVID-19 DataSharing/BR.
+Python package `defend-covid-severity` to generate severity-prediction views of
+the datasets provided by FAPESP COVID-19 DataSharing/BR.
 """
 
 def do_nothing():
