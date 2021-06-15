604301681936ef9b6fcc0dd72a6eed4385fedc87
Filipe Verri <filipeverri@gmail.com>
2021-06-15 12:53:05 -0300

    Implemented read_tests.

--- src/covidseverity/__init__.py
+++ src/covidseverity/__init__.py
@@ -8,32 +8,45 @@ import pandas as pd
 
 def read_patients(filepath_or_buffer, sep='|'):
     """
-    Read and organizes patients file.
+    Reads and organizes patients file.
     """
     data = pd.read_table(filepath_or_buffer, sep=sep)
     assert data.shape[1] == 7
 
     data.rename(str.upper, axis='columns', inplace=True)
-    data.rename(columns={
-        'ID_PACIENTE': 'patient',
-        'IC_SEXO': 'sex',
-        'AA_NASCIMENTO': 'birth_year',
-        'CD_PAIS': 'country',
-        'CD_UF': 'state',
-        'CD_MUNICIPIO': 'city',
-        'CD_CEPREDUZIDO': 'postal_code'}, inplace=True)
 
-    for column in ['birth_year', 'postal_code']:
+    for column in ['AA_NASCIMENTO']:
         data[column] = pd.to_numeric(
             data[column], errors='coerce', downcast='integer')
 
     missing_values = [
-        ('country', 'XX'),
-        ('state', 'UU'),
-        ('city', 'MMMM'),
+        ('CD_PAIS', 'XX'),
+        ('CD_UF', 'UU'),
+        ('CD_MUNICIPIO', 'MMMM'),
+        ('CD_CEPREDUZIDO', 'CCCC')
     ]
 
     for column, value in missing_values:
-        data[column].loc[data[column] == value] = None
+        data.loc[data[column] == value, column] = None
+
+    return data
+
+
+def read_tests(filepath_or_buffer, sep='|'):
+    """
+    Reads and organizes tests file.
+    """
+    data = pd.read_table(filepath_or_buffer, sep=sep)
+    assert data.shape[1] == 9
+
+    data.rename(str.upper, axis='columns', inplace=True)
+
+    nominal_columns = [col for col in data if col.startswith('DE_')]
+    for column in nominal_columns:
+        data[column] = data[column].str.strip()
+
+    date_columns = [col for col in data if col.startswith('DT_')]
+    for column in date_columns:
+        data[column] = pd.to_datetime(data[column], format='%d/%m/%Y')
 
     return data
