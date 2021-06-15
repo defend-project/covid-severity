25483bbb7cc0b6ddc43e8cccb925382e9c5db09d
Filipe Verri <filipeverri@gmail.com>
2021-06-15 10:54:34 -0300

    Preparing package.

---
+++ src/covidseverity/__init__.py
@@ -0,0 +1,10 @@
+"""
+Python package `covidseverity` to generate severity-prediction views of the
+datasets provided by FAPESP COVID-19 DataSharing/BR.
+"""
+
+def do_nothing():
+    """
+    Something that does nothing.
+    """
+    pass
